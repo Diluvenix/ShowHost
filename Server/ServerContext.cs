bfb916d88c344f566f4787cb4db7beb2701f2d80
Diluvenix <146968909+Diluvenix@users.noreply.github.com>
2026-09-01 11:51:12 +0200

    improves ServerContext class

--- Server/ServerContext.cs
+++ Server/ServerContext.cs
@@ -7,30 +7,24 @@ namespace Server
 {
     internal static class ServerContext
     {
-        public static ConcurrentDictionary<string, Player> Players = [];
+        public static readonly ConcurrentDictionary<string, Player> Players = [];
 
-        public static KeyManager ModeratorKeys = new();
+        public static readonly KeyManager ModeratorKeys = new();
 
-        public static Lobby Lobby = new();
+        public static readonly Lobby Lobby = new();
         public static readonly ConcurrentNameDictionary<IService> Services = new(
             NameGenerator.Generate,
             (s) => !string.IsNullOrEmpty(s) && s.Length <= 32 && s.AsSpan().IndexOfAnyExceptInRange(' ', '~') == -1
         );
 
-        public static readonly CancellationTokenSource Cts = new();
-
         public static void Dispose()
         {
-            Cts.Cancel();
-
             Lobby.Dispose();
             foreach (IService service in Services.Values)
                 service.Dispose();
 
             foreach (Player player in Players.Values)
                 player.Dispose();
-
-            Cts.Dispose();
         }
     }
 }
